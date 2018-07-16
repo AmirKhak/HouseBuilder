7d1eaa86f2c57af4963bf1debc17cb894da340cb
ak8815 <ak8815@ic.ac.uk>
2018-07-17 00:19:11 +0100

    Implemented Order page.

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -1,1 +1,56 @@
-<div id="orders_body"></div>
+@extends('layouts.app')
+@section('content')
+  <div id="orders_body">
+    <h2>Choose the section you need</h2>
+    <div id="options_container">
+      <div id="option_1" class="option_container">
+        <div id="option_1_dropdown">
+          <div class="option_dropdown_link">
+            <button>Interior and exterior painting</button>
+          </div>
+        </div>
+        <p>Interior and exterior painting</p>
+      </div>
+      <div id="option_2" class="option_container">
+        <div id="option_2_dropdown">
+          <div class="option_dropdown_link">
+            <button>Building conservatory and extension</button>
+          </div>
+        </div>
+        <p>Building conservatory and extension</p>
+      </div>
+      <div id="option_3" class="option_container">
+        <div id="option_3_dropdown">
+          <div class="option_dropdown_link">
+            <button>Plumbing and electrical wiring</button>
+          </div>
+        </div>
+        <p>Plumbing and electrical wiring</p>
+      </div>
+      <div id="option_4" class="option_container">
+        <div id="option_4_dropdown">
+          <div class="option_dropdown_link">
+            <button>Desiging and paving house front</button>
+          </div>
+        </div>
+        <p>Desiging and paving house front</p>
+      </div>
+      <div id="option_5" class="option_container">
+        <div id="option_5_dropdown">
+          <div class="option_dropdown_link">
+            <button>Installing Laminet flooring</button>
+          </div>
+        </div>
+        <p>Installing Laminet flooring</p>
+      </div>
+      <div id="option_6" class="option_container">
+        <div id="option_6_dropdown">
+          <div class="option_dropdown_link">
+            <button>Other house related works</button>
+          </div>
+        </div>
+        <p>Other house related works</p>
+      </div>
+    </div>
+  </div>
+@endsection
