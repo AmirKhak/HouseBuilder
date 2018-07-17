894909789127d65deced5c4c0298bd54d301cd3f
ak8815 <ak8815@ic.ac.uk>
2018-07-17 16:28:56 +0100

    Implemented Contact View

--- resources/views/pages/about_us.blade.php
+++ resources/views/pages/about_us.blade.php
@@ -2,6 +2,7 @@
 @section('content')
   <div id="about_us_body">
     <div id="about_us_desc">
+      <h2>Who We Are</h2>
       <p>We accpet wide range of house repairs. We have had more than 30 years of experience in house decoration and building.
       We have both knowledge and experience to do wide range of house related work with a really reasonable price.
       Also we are very flexible and friendly with our customers and have had a satisfactory feedback.
--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -1,6 +1,25 @@
 @extends('layouts.app')
 @section('content')
   <div id="contact_body">
-    
+    <div id="contact_body_topbox">
+      <h2>Contact</h2>
+    </div>
+    <div id="contact_body_lowerbox"></div>
+    <div id="contact_body_content">
+      <div id="contact_body_content_left">
+        <img src="/images/east_london_map.png">
+      </div>
+      <div id="contact_body_content_right">
+        <h2>Email</h2>
+        <p>info@HouseBuiler.com</p>
+        <h2>Address</h2>
+        <p>Hedgemans Rd</p>
+        <p>Dagenham</p>
+        <p>London</p>
+        <p>RM9 6DL</p>
+        <h2>Telephone</h2>
+        <p>020 8995 3575</p>
+      </div>
+    </div>
   </div>
 @endsection
